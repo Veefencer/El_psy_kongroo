819180974de00deadbb283553757053ee1df4d3e
Vlad Tsvetkov <79256641+Veefencer@users.noreply.github.com>
2021-06-17 15:18:51 +0300

    Update test_filecomparison.py

--- Tests/test_filecomparison.py
+++ Tests/test_filecomparison.py
@@ -1,20 +1,20 @@
 import unittest
-import filecomp
+from filecomp import fcomp
 
 
 class MyTestCase(unittest.TestCase):
     def test_empty_folder(self):
-        self.assertEqual(filecomp.fcomp("D:/test_folder_empty"), None)
+        self.assertEqual(fcomp("D:/test_folder_empty"), None)
 
     def test_regular_1(self):
-        self.assertEqual(filecomp.fcomp("D:/test_folder")["maxs"][0], "D:/test_folder/expected")
+        self.assertEqual(fcomp("D:/test_folder")["maxs"][0], "D:/test_folder/expected")
 
     def test_regular_2(self):
-        self.assertEqual(filecomp.fcomp("D:\\test_folder")["mins"][0], "D:/test_folder/загружено.htm")
+        self.assertEqual(fcomp("D:\\test_folder")["mins"][0], "D:/test_folder/загружено.htm")
 
     @unittest.expectedFailure
     def test_expected_failure(self):
-        self.assertEqual(filecomp.fcomp("D:\test_folder")["maxs"][0], "D:/test_folder/expected")
+        self.assertEqual(fcomp("D:\test_folder")["maxs"][0], "D:/test_folder/expected")
 
 
 if __name__ == '__main__':
